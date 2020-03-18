4fab7b015116aef681c6abf2f5d0fca1ae566495
Jason Meli <jdmmeli@yahoo.com>
2020-03-17 21:46:28 -0400

    uv index api call

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$("#button").on("click" , function() {
+function getInfo() {
     var APIkey = "aa448693cc6b5b40967cafecafdd7c15"
     var city = $("#city").val().trim();
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
@@ -8,7 +8,19 @@ $("#button").on("click" , function() {
         method: "GET"
       })
       .then(function(response) {
+        var result = response
+        console.log(result);
+
+        var queryURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIkey + "&lat=" + result.coord.lat + "&lon=" + result.coord.lon;
+    
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+      .then(function(response) {
         console.log(response);
+      });
+
       });
     
       var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIkey;
@@ -20,5 +32,15 @@ $("#button").on("click" , function() {
       .then(function(response) {
         console.log(response);
       });
+
+     
+}
+
+$("#button").on("click", function(event) {
+    event.preventDefault();
+    getInfo()
+   
+    })
     
-})
+
+    
